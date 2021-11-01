c2554847c38d39dc922e949067299de8953ea669
Nadezhda Anisimova <nadya.anisimova@gmail.com>
2021-11-02 00:37:18 +0300

    change linter option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,4 +5,3 @@ const program = new Command();
 program.version('0.0.1');
 program.description('Compares two configuration files and shows a difference.');
 program.parse(process.argv);
-const options = program.opts();
