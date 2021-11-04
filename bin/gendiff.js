fe31adf4bb3832e53a7386c6da72dd58c2ba06e5
Nadezhda Anisimova <nadya.anisimova@gmail.com>
2021-11-04 22:35:47 +0300

    add 3 step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 import { Command } from 'commander/esm.mjs';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format');
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
 program.parse(process.argv);
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import { cwd } from 'process';
+import fileComparison from '../src/compare.js';
+
+const loadFile = (filepath) => {
+  const realPath = path.resolve(cwd(), './fixtures/', filepath);
+  const normalFile = JSON.parse(fs.readFileSync(realPath));
+  return normalFile;
+};
+
+const genDiff = (filepath1, filepath2) => {
+  const file1 = loadFile(filepath1);
+  const file2 = loadFile(filepath2);
+  return fileComparison(file1, file2);
+};
+
+export default genDiff;
